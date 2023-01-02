90836cbcccbca599cf507220b7ee9fe8c0f52226
padmanvinay <vinaypadman111@gmail.com>
2023-01-02 14:34:24 +0530

    fifth

--- dotnet-rpg/models/Character.cs
+++ dotnet-rpg/models/Character.cs
@@ -8,7 +8,7 @@ namespace dotnet_rpg.models
     public class Character
     {
         public int Id { get; set; }
-        public string Name { get; set; } 
+        public string? Name { get; set; } 
         public int HitPoints { get; set; } 
         public int Strength { get; set; } 
         public int Defense { get; set; } 
